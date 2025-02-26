c161246699231e708b315454c90156b240d43a8c
Gannio <thegreatgannio@gmail.com>
2025-02-25 20:30:06 -0700

    Something I needed to compile this I think.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd build
-g++ -std=c++17 -march=native -g -maes -x c++ ../listUnreferenced.cpp -ogmslur
+g++ -std=c++17 -march=native -g -maes -x c++ ../listUnreferenced.cpp -ogmslur -static-libstdc++
 #g++ -std=c++17 -march=native -g -maes -x c++ ../listUnreferenced.cpp
